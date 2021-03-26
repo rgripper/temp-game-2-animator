e2ccfabee6aa0b38481012ec0e856479f3ebbd49
Vladimir <azuresplash@gmail.com>
2021-03-26 09:27:18 +0100

    Fixed basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,108 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import React, { useCallback, useState } from 'react';
 import './App.css';
+import * as posenet from '@tensorflow-models/posenet';
+import '@tensorflow/tfjs-converter';
+import '@tensorflow/tfjs-core';
+import '@tensorflow/tfjs-backend-webgl';
+import '@tensorflow/tfjs-backend-cpu';
 
-interface AppProps {}
+type AppProps = {};
 
 function App({}: AppProps) {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
+  // const videoRef = useRef<HTMLVideoElement | null>(null)
+  const [keypoints, setKeypoints] = useState<posenet.Keypoint[]>([]);
+  const [resolution, setResolution] = useState<{
+    width: number;
+    height: number;
+  } | null>(null);
+
+  const setVideo = useCallback((video: HTMLVideoElement | null) => {
+    if (!video) return;
+
+    const constraints = {
+      video: true,
+    };
+
+    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+      video.srcObject = stream;
+    });
+  }, []);
+
+  const startEstimation = useCallback(
+    async (event: React.SyntheticEvent<HTMLVideoElement, Event>) => {
+      const video = event.currentTarget;
+      const inputResolution = {
+        width: video.videoWidth,
+        height: video.videoHeight,
+      };
+
+      video.width = inputResolution.width;
+      video.height = inputResolution.height;
+      console.log('resolution', inputResolution);
+      const net = await posenet.load({
+        architecture: 'ResNet50',
+        outputStride: 32,
+        inputResolution,
+        quantBytes: 2,
+      });
+
+      setResolution(inputResolution);
+
+      setInterval(async () => {
+        const pose = await net.estimateSinglePose(video, {
+          flipHorizontal: false,
+        });
+
+        setKeypoints(pose.keypoints);
+      }, 1000);
+    },
+    [],
+  );
+
+  const divSize = resolution && {
+    width: resolution.width + 'px',
+    height: resolution.height + 'px',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div className="App" style={{ position: 'relative' }}>
+      <div
+        style={{
+          position: 'absolute',
+          width: '500px',
+          height: '500px',
+          ...(divSize || {}),
+        }}
+      >
+        {resolution &&
+          keypoints.map((keypoint) => {
+            const hasPosition = !!(keypoint.position.x && keypoint.position.y);
+            //hasPosition &&
+            //console.log(resolution, keypoint.part, keypoint.position);
+            return (
+              hasPosition && (
+                <div
+                  key={keypoint.part}
+                  title={keypoint.part}
+                  style={{
+                    position: 'absolute',
+                    left: Math.ceil(keypoint.position.x) + 'px',
+                    top: Math.ceil(keypoint.position.y) + 'px',
+                    width: '10px',
+                    height: '10px',
+                    background: 'green',
+                  }}
+                />
+              )
+            );
+          })}
+      </div>
+      <video
+        id="vid"
+        ref={setVideo}
+        onLoadedData={startEstimation}
+        autoPlay
+      ></video>
     </div>
   );
 }
---
+++ src/pose.ts
@@ -0,0 +1,23 @@
+import posenet from '@tensorflow-models/posenet';
+
+// async function estimatePoseOnImage(imageElement: HTMLImageElement) {
+//   // load the posenet model from a checkpoint
+//   //  const net = await posenet.load();
+//   const net = await posenet.load({
+//     architecture: 'ResNet50',
+//     outputStride: 32,
+//     inputResolution: { width: 257, height: 200 },
+//     quantBytes: 2,
+//   });
+
+//   const pose = await net.estimateSinglePose(imageElement, {
+//     flipHorizontal: false,
+//   });
+//   return pose;
+// }
+
+// const imageElement = document.getElementById('cat');
+
+// const pose = estimatePoseOnImage(imageElement!);
+
+// console.log(pose);
