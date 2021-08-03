b24f7daa912edfa7a21039d69b732490309404e1
Vladimir <azuresplash@gmail.com>
2021-08-03 21:12:50 +0200

    Fixed alignment of the head

--- src/Animator.tsx
+++ src/Animator.tsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/css';
 import React from 'react';
 import type { Point, Pose } from './FrameEstimator';
 
@@ -11,7 +12,20 @@ export function Animator({ poses }: { poses: Pose[] }) {
   );
 }
 
-type KeypointMap = Record<
+const bodyStyles = css`
+  line {
+    stroke: black;
+    stroke-width: 5;
+  }
+
+  * {
+    opacity: 0.5;
+  }
+`;
+
+type CharacterDirection = 'right' | 'left' | 'front' | 'back';
+
+export type KeypointMap = Record<
   | 'nose'
   | 'left_eye'
   | 'right_eye'
@@ -33,95 +47,269 @@ type KeypointMap = Record<
 >;
 
 function FramePose({ pose }: { pose: Pose }) {
-  const keypointMap = (Object.fromEntries(
-    pose.keypoints.map((keypoint) => [keypoint.name, { x: keypoint.x, y: keypoint.y, z: keypoint.z }]),
-  ) as unknown) as KeypointMap;
+  const originalKeypointMap = getKeypointMap(pose);
+  const keypointMap = normalizeMap(originalKeypointMap);
+  const torsoRect = {
+    x1: (keypointMap.left_shoulder.x + keypointMap.left_hip.x) / 2,
+    x2: (keypointMap.right_shoulder.x + keypointMap.right_hip.x) / 2,
+    y1: (keypointMap.left_shoulder.y + keypointMap.right_shoulder.y) / 2,
+    y2: (keypointMap.left_hip.y + keypointMap.right_hip.y) / 2,
+  };
 
+  const torsoTopMidX = (torsoRect.x1 + torsoRect.x2) / 2;
+  const characterDirection = keypointMap.nose.x < torsoTopMidX ? 'left' : 'right';
+
+  console.log({
+    characterDirection,
+    right: keypointMap.right_ear.x,
+    nose: keypointMap.nose.x,
+    left: keypointMap.left_ear.x,
+  });
   return (
-    <svg viewBox="0 0 500 500" width="200" height="200" xmlns="http://www.w3.org/2000/svg">
+    <svg className={bodyStyles} viewBox="0 0 800 800" width="500" height="500" xmlns="http://www.w3.org/2000/svg">
+      <Head keypointMap={keypointMap} characterDirection={characterDirection} />
+      <Neck keypointMap={keypointMap} torsoRect={torsoRect} />
       {/** Neck */}
-      <line
-        x1={keypointMap.left_shoulder.x + (keypointMap.right_shoulder.x - keypointMap.left_shoulder.x) / 2}
-        y1={(keypointMap.left_shoulder.y + keypointMap.right_shoulder.y) / 2}
-        x2={keypointMap.nose.x}
-        y2={keypointMap.nose.y}
-        stroke="black"
-      />
       <line
         x1={keypointMap.left_shoulder.x}
         y1={keypointMap.left_shoulder.y}
         x2={keypointMap.right_shoulder.x}
         y2={keypointMap.right_shoulder.y}
-        stroke="black"
       />
       {/** Left arm */}
+      <Limb
+        x1={keypointMap.left_shoulder.x}
+        y1={keypointMap.left_shoulder.y}
+        x2={keypointMap.left_elbow.x}
+        y2={keypointMap.left_elbow.y}
+      />
       <line
         x1={keypointMap.left_shoulder.x}
         y1={keypointMap.left_shoulder.y}
         x2={keypointMap.left_elbow.x}
         y2={keypointMap.left_elbow.y}
-        stroke="black"
       />
       <line
         x1={keypointMap.left_elbow.x}
         y1={keypointMap.left_elbow.y}
         x2={keypointMap.left_wrist.x}
         y2={keypointMap.left_wrist.y}
-        stroke="black"
       />
       {/** Right arm */}
+      <Limb
+        x1={keypointMap.right_shoulder.x}
+        y1={keypointMap.right_shoulder.y}
+        x2={keypointMap.right_elbow.x}
+        y2={keypointMap.right_elbow.y}
+      />
       <line
         x1={keypointMap.right_shoulder.x}
         y1={keypointMap.right_shoulder.y}
         x2={keypointMap.right_elbow.x}
         y2={keypointMap.right_elbow.y}
-        stroke="black"
       />
       <line
         x1={keypointMap.right_elbow.x}
         y1={keypointMap.right_elbow.y}
         x2={keypointMap.right_wrist.x}
         y2={keypointMap.right_wrist.y}
-        stroke="black"
       />
+      <Torso torsoRect={torsoRect} />
       {/** Pelvis */}
       <line
         x1={keypointMap.left_hip.x}
         y1={keypointMap.left_hip.y}
         x2={keypointMap.right_hip.x}
         y2={keypointMap.right_hip.y}
-        stroke="black"
       />
       {/** Left leg */}
+      <Limb
+        x1={keypointMap.left_hip.x}
+        y1={keypointMap.left_hip.y}
+        x2={keypointMap.left_knee.x}
+        y2={keypointMap.left_knee.y}
+      />
       <line
         x1={keypointMap.left_hip.x}
         y1={keypointMap.left_hip.y}
         x2={keypointMap.left_knee.x}
         y2={keypointMap.left_knee.y}
-        stroke="black"
       />
       <line
         x1={keypointMap.left_knee.x}
         y1={keypointMap.left_knee.y}
         x2={keypointMap.left_ankle.x}
         y2={keypointMap.left_ankle.y}
-        stroke="black"
       />
       {/** Left leg */}
+      <Limb
+        x1={keypointMap.right_hip.x}
+        y1={keypointMap.right_hip.y}
+        x2={keypointMap.right_knee.x}
+        y2={keypointMap.right_knee.y}
+      />
       <line
         x1={keypointMap.right_hip.x}
         y1={keypointMap.right_hip.y}
         x2={keypointMap.right_knee.x}
         y2={keypointMap.right_knee.y}
-        stroke="black"
       />
       <line
         x1={keypointMap.right_knee.x}
         y1={keypointMap.right_knee.y}
         x2={keypointMap.right_ankle.x}
         y2={keypointMap.right_ankle.y}
-        stroke="black"
       />
     </svg>
   );
+}
+
+function getHeadX(keypointMap: KeypointMap, characterDirection: CharacterDirection, width: number) {
+  switch (characterDirection) {
+    case 'left':
+      return keypointMap.nose.x;
+    case 'right':
+      return keypointMap.nose.x + width;
+    case 'front':
+    case 'back':
+      return keypointMap.nose.x - width / 2;
+  }
+}
+
+function Head({
+  keypointMap,
+  characterDirection,
+}: {
+  keypointMap: KeypointMap;
+  characterDirection: CharacterDirection;
+}) {
+  const width = 40; // Math.abs(keypointMap.right_ear.x - keypointMap.left_ear.x) + 5;
+  const midX = (keypointMap.right_ear.x + keypointMap.left_ear.x) / 2;
+  return <rect x={midX - width / 2} y={keypointMap.nose.y - width / 2} width={width} height={width} fill="blue"></rect>;
+}
+
+function Neck({ keypointMap, torsoRect }: { keypointMap: KeypointMap; torsoRect: Rectangle }) {
+  return (
+    <line x1={(torsoRect.x1 + torsoRect.x2) / 2} y1={torsoRect.y1} x2={keypointMap.nose.x} y2={keypointMap.nose.y} />
+  );
+}
+
+type Rectangle = {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+};
+
+function Torso({ torsoRect }: { torsoRect: Rectangle }) {
+  const width = Math.abs(torsoRect.x1 - torsoRect.x2);
+  const height = Math.abs(torsoRect.y1 - torsoRect.y2);
+
+  return (
+    <rect
+      x={(torsoRect.x1 + torsoRect.x2) / 2}
+      y={(torsoRect.y1 + torsoRect.y2) / 2}
+      width={width}
+      height={height}
+      transform={`translate(-${width / 2} -${height / 2})`}
+      fill="pink"
+    ></rect>
+  );
+}
+
+function Limb({ x1, y1, x2, y2 }: { x1: number; y1: number; x2: number; y2: number }) {
+  const length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+  const angleDeg = (Math.atan2(y2 - y1, x2 - x1) * 180) / Math.PI - 90;
+  return (
+    <g transform={`translate(${x1} ${y1}) rotate(${angleDeg})`}>
+      <rect width={20} height={length} fill="red" transform={`translate(-10 0)`}></rect>
+    </g>
+  );
+}
+
+export function getKeypointMap(pose: Pose): KeypointMap {
+  return Object.fromEntries(
+    pose.keypoints.map((keypoint) => [keypoint.name, { x: keypoint.x, y: keypoint.y, z: keypoint.z }]),
+  ) as unknown as KeypointMap;
+}
+
+function normalizeMap(keypointMap: KeypointMap): KeypointMap {
+  const torsoRect = {
+    x1: (keypointMap.left_shoulder.x + keypointMap.left_hip.x) / 2,
+    x2: (keypointMap.right_shoulder.x + keypointMap.right_hip.x) / 2,
+    y1: (keypointMap.left_shoulder.y + keypointMap.right_shoulder.y) / 2,
+    y2: (keypointMap.left_hip.y + keypointMap.right_hip.y) / 2,
+  };
+
+  const torsoParts = {
+    left_shoulder: {
+      x: torsoRect.x1,
+      y: torsoRect.y1,
+    },
+    right_shoulder: {
+      x: torsoRect.x2,
+      y: torsoRect.y1,
+    },
+    left_hip: {
+      x: torsoRect.x1,
+      y: torsoRect.y2,
+    },
+    right_hip: {
+      x: torsoRect.x2,
+      y: torsoRect.y2,
+    },
+  };
+
+  const upperShiftX =
+    (torsoParts.left_shoulder.x -
+      keypointMap.left_shoulder.x +
+      (torsoParts.right_shoulder.x - keypointMap.right_shoulder.x)) /
+    2;
+  const upperShiftY =
+    (torsoParts.left_shoulder.y -
+      keypointMap.left_shoulder.y +
+      (torsoParts.right_shoulder.y - keypointMap.right_shoulder.y)) /
+    2;
+
+  const lowerShiftX =
+    (torsoParts.left_hip.x - keypointMap.left_hip.x + (torsoParts.right_hip.x - keypointMap.right_hip.x)) / 2;
+  const lowerShiftY =
+    (torsoParts.left_hip.y - keypointMap.left_hip.y + (torsoParts.right_hip.y - keypointMap.right_hip.y)) / 2;
+
+  return {
+    ...keypointMap, // TODO
+    ...torsoParts,
+    nose: {
+      x: keypointMap.nose.x + upperShiftX,
+      y: keypointMap.nose.y + upperShiftY,
+    },
+    left_elbow: {
+      x: keypointMap.left_elbow.x + upperShiftX,
+      y: keypointMap.left_elbow.y + upperShiftY,
+    },
+    right_elbow: {
+      x: keypointMap.right_elbow.x + upperShiftX,
+      y: keypointMap.right_elbow.y + upperShiftY,
+    },
+    left_wrist: {
+      x: keypointMap.left_wrist.x + upperShiftX,
+      y: keypointMap.left_wrist.y + upperShiftY,
+    },
+    right_wrist: {
+      x: keypointMap.right_wrist.x + upperShiftX,
+      y: keypointMap.right_wrist.y + upperShiftY,
+    },
+    left_ear: {
+      x: keypointMap.left_ear.x + upperShiftX,
+      y: keypointMap.left_ear.y + upperShiftY,
+    },
+    left_ankle: {
+      x: keypointMap.left_ankle.x + lowerShiftX,
+      y: keypointMap.left_ankle.y + lowerShiftY,
+    },
+    right_ankle: {
+      x: keypointMap.right_ankle.x + lowerShiftX,
+      y: keypointMap.right_ankle.y + lowerShiftY,
+    },
+  };
 }
--- src/Dresser.tsx
+++ src/Dresser.tsx
@@ -1,20 +1,61 @@
 import type { Pose } from './FrameEstimator';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getKeypointMap } from './Animator';
 
 export function Dresser({ pose }: { pose: Pose }) {
-  const range = (count: number) => new Array(count).fill(null);
+  const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
 
-  return (
-    <div>
-      {range(16).map((y, yi) => (
-        <div key={yi}>
-          {range(16).map((x, xi) => (
-            <span key={xi} style={{ width: '24px', height: '24px', display: 'inline-block' }}>
-              {x},{y}
-            </span>
-          ))}
-        </div>
-      ))}
-    </div>
+  useEffect(() => {
+    if (canvas) {
+      renderPose(canvas, pose);
+    }
+  }, [canvas, pose]);
+
+  return <canvas ref={setCanvas} />;
+  // const range = (count: number) => new Array(count).fill(null);
+
+  // return (
+  //   <div>
+  //     {range(16).map((y, yi) => (
+  //       <div key={yi}>
+  //         {range(16).map((x, xi) => (
+  //           <span key={xi} style={{ width: '24px', height: '24px', display: 'inline-block' }}>
+  //             {x},{y}
+  //           </span>
+  //         ))}
+  //       </div>
+  //     ))}
+  //   </div>
+  // );
+}
+function renderPose(canvas: HTMLCanvasElement, pose: Pose) {
+  const ctx = canvas.getContext('2d')!;
+  const keypointMap = getKeypointMap(pose);
+
+  ctx.moveTo(
+    keypointMap.left_shoulder.x + (keypointMap.right_shoulder.x - keypointMap.left_shoulder.x) / 2,
+    (keypointMap.left_shoulder.y + keypointMap.right_shoulder.y) / 2,
   );
+  ctx.lineTo(keypointMap.nose.x, keypointMap.nose.y);
+
+  ctx.moveTo(keypointMap.left_shoulder.x, keypointMap.left_shoulder.y);
+  ctx.lineTo(keypointMap.right_shoulder.x, keypointMap.right_shoulder.y);
+
+  ctx.moveTo(keypointMap.left_shoulder.x, keypointMap.left_shoulder.y);
+  ctx.lineTo(keypointMap.left_elbow.x, keypointMap.left_elbow.y);
+
+  ctx.moveTo(keypointMap.left_elbow.x, keypointMap.left_elbow.y);
+  ctx.lineTo(keypointMap.left_wrist.x, keypointMap.left_wrist.y);
+
+  ctx.moveTo(keypointMap.right_shoulder.x, keypointMap.right_shoulder.y);
+  ctx.lineTo(keypointMap.right_elbow.x, keypointMap.right_elbow.y);
+
+  ctx.moveTo(keypointMap.right_elbow.x, keypointMap.right_elbow.y);
+  ctx.lineTo(keypointMap.right_wrist.x, keypointMap.right_wrist.y);
+
+  ctx.moveTo(keypointMap.left_hip.x, keypointMap.left_hip.y);
+  ctx.lineTo(keypointMap.right_hip.x, keypointMap.right_hip.y);
+
+  ctx.moveTo(keypointMap.left_hip.x, keypointMap.left_hip.y);
+  ctx.lineTo(keypointMap.right_hip.x, keypointMap.right_hip.y);
 }
